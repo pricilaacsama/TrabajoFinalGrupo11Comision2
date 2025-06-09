c864f17d93afabb000513beae1cd2c8f9a3a7a41
pricila acsama <pricilaacsama12@gmail.com>
2025-06-09 17:51:44 -0300

    Pricila Acsama - agregando una pagina de error

--- src/assets/pages/Error.jsx
+++ src/assets/pages/Error.jsx
@@ -5,9 +5,9 @@ function Error() {
   return (
     <Container className="text-center my-5">
       <h1 className="display-1 text-danger fw-bold">404</h1>
-      <h2 className="mb-4">Página no encontrada</h2>
+      <h2 className="mb-4">Pagina no encontrada</h2>
       <p className="text-muted mb-4">
-        Lo sentimos, la página que estás buscando no existe o fue movida.
+        Lo sentimos, la pagina que estas buscando no existe o esta mal escrito.
       </p>
       <Button as={Link} to="/" variant="primary">
         Volver al inicio
