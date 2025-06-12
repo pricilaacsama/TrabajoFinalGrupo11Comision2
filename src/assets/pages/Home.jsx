162548cd3ed230abc31dfd6ff3f2a86269eeca3c
JuanLamas <juanedulam2@gmail.com>
2025-06-12 09:36:24 -0300

    Cambiando el uso de props para pasar la lista de producto por contextProduct

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,11 +1,11 @@
 import ProductList from "../components/ProductList";
 import { Container } from "react-bootstrap";
 
-function Home({ productos }) {
+function Home() {
   return (
     <Container className="my-4">
       <h1 className="text-center mb-4 display-4 fw-bold text-dark">Bienvenidos...</h1>
-      <ProductList productos={productos} />
+      <ProductList/>
     </Container>
   );
 }
