620f2ffa805d1ad623e2fb8f22fe49a2e8c6995a
pricila acsama <pricilaacsama12@gmail.com>
2025-06-25 02:31:00 -0300

    Pricila Acsama - agregando el registrar usuario y msj de bienvenida

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import Error from './assets/pages/Error';
 import Acerca from './assets/pages/Acerca';
 import Detalle from './assets/pages/Detalle';
 import Formulario from './assets/components/Formulario';
-import Login from './assets/pages/login'; // Asegúrate que el archivo se llame igual (Login.js o login.js)
+import Login from './assets/pages/login';
 import './App.css';
 import ProtectedRoute from './assets/components/ProtectedRoute';
 import Footer from './assets/components/Footer';
 import {ValidarToken} from './assets/components/Token'
+import Registro from './assets/components/Registro';
 
 function App() {
 
@@ -27,6 +28,8 @@ function App() {
 
         {/* Pagina de token */}
         <Route path="/token" element={<ValidarToken />} />
+        {/*pagina de registrar usuario*/}
+        <Route path="/registrar" element={<Registro/>} />
 
         {/*acceso para ambos roles */}
         <Route
@@ -78,7 +81,6 @@ function App() {
             </ProtectedRoute>
           }
         />
-
         {/* Ruta por defecto para errores */}
         <Route path="*" element={<Error />} />
       </Routes>
@@ -88,44 +90,3 @@ function App() {
 }
 
 export default App;
-
-
-
-/*
-<Route path="/home" element={<Home productos={productos} />} />
-
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './assets/components/NavBar';
-import Home from './assets/pages/Home';
-import Favoritos from './assets/pages/Favoritos';
-import productosData from './assets/data/productos.json';
-import Error from './assets/pages/Error';
-import Acerca from './assets/pages/Acerca';
-import Detalle from './assets/pages/Detalle'; // ✅ AÑADIR
-import Formulario from './assets/components/Formulario'
-import './App.css';
-import { useState } from 'react';
-
-import Login from './assets/pages/login'; // ✅
-
-function App() {
-  const [productos, setProductos] = useState(productosData);
-
-  return (
-    <>
-      <NavBar />
-      <Routes>
-        <Route path="/login" element={<Login />} /> 
-        <Route path="/" element={<Home productos={productos} />} />
-        <Route path="/favoritos" element={<Favoritos />} />
-        <Route path="/crear" element={<Formulario productos={productos}/>} />
-        <Route path="/editar/:id" element={<Formulario />} />
-        <Route path="/acerca" element={<Acerca />} />
-        <Route path="/detalle/:id" element={<Detalle />} /> 
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </>
-  );
-}
-
-export default App;*/
